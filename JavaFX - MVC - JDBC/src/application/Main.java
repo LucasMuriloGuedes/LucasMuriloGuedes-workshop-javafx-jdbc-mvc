d1caf252ec3e76a69f66069711832683443e45fe
LUCAS MURILO GUEDES OLIVEIRA <lucasmurilo.guedes@gmail.com>
2021-04-26 18:02:33 -0400

    Enviando atividades do curso de java para o github

---
+++ JavaFX - MVC - JDBC/src/application/Main.java
@@ -0,0 +1,50 @@
+package application;
+
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+
+public class Main extends Application {
+    
+    private static Scene mainScene;
+    
+    @Override
+    public void start(Stage primaryStage) {
+        
+        
+        try{
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+            ScrollPane scrollPane = loader.load();
+        
+            scrollPane.setFitToHeight(true); // para que barra de menu fique do tamanho da tela
+            scrollPane.setFitToWidth(true); // para que barra de menu fique do tamanho da tela
+            
+            mainScene = new Scene(scrollPane);
+            
+            primaryStage.setScene(mainScene);
+            primaryStage.setTitle("Java FX - Lucas Murilo");
+            primaryStage.show();
+            
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
---
+++ JavaFX - MVC - JDBC/src/gui/MainViewController.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.MenuItem;
+
+/**
+ * FXML Controller class
+ *
+ * @author Lucas Murilo
+ */
+public class MainViewController implements Initializable {
+
+    @FXML
+    private MenuItem menuItemSeller;
+    @FXML
+    private MenuItem menuItemDepartment;
+    @FXML
+    private MenuItem menuItemAbout;
+    
+    @FXML
+    
+    public void onMenuItemSellerAction(){
+        
+    }
+    
+    public void onMenuitemDepartamentAction(){
+        
+        
+    }
+    
+    public void onMenuItemAboutAction(){
+        
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
