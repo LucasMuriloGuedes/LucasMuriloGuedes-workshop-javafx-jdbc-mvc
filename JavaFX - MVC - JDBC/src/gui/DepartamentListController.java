66eec5a93365f45fea7b04ac17b3e3a705594d4b
LUCAS MURILO GUEDES OLIVEIRA <lucasmurilo.guedes@gmail.com>
2021-04-26 18:55:25 -0400

    DepartmentList Design

--- JavaFX - MVC - JDBC/src/gui/DepartamentListController.java
+++ JavaFX - MVC - JDBC/src/gui/DepartamentListController.java
@@ -5,9 +5,17 @@
  */
 package gui;
 
+import application.Main;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
+import model.entities.Department;
 
 /**
  * FXML Controller class
@@ -16,12 +24,32 @@ import javafx.fxml.Initializable;
  */
 public class DepartamentListController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Button btnNew;
+    @FXML
+    private TableView<Department> tableViewDepartament;
+    @FXML
+    private TableColumn<Department, Integer> tableColumnId;
+    @FXML
+    private TableColumn<Department, String> tableColumnName;
+    
+    @FXML
+    public void onBtnNewAction(){
+        
+    }
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        initializeNode();
     }    
+
+    private void initializeNode() {
+        tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        tableColumnName.setCellValueFactory(new PropertyValueFactory<>("name"));
+        
+        Stage stage = (Stage) Main.getMaiScene().getWindow(); // para a tabela acompanhar a altura da janela
+        tableViewDepartament.prefHeightProperty().bind(stage.heightProperty()); // para a tabela acompanhar a altura da janelasa
+
+    }
     
 }
